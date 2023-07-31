0a8dbb172631460657496c9632c4e11af4800bf0
demanghon <34471617+demanghon@users.noreply.github.com>
2023-07-31 16:33:56 +0200

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { ApplicationModule } from './app.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [ApplicationModule],
+  imports: [CommonModule, ApplicationModule],
   template: `
   <h1> Content projection</h1>
   <br>
@@ -27,16 +28,37 @@ import { ApplicationModule } from './app.module';
   </app-blog-post>
   <br> 
   <h1>ng template</h1> 
+
+  <ng-template #template1 let-name="name">
+    Template info...
+  </ng-template>
   <ng-container *ngTemplateOutlet="template1">
     Container info...
   </ng-container>
-  <ng-template #template1>
-    Template info...
+
+  <h1>ng template with context</h1> 
+
+  <ng-template #template2 let-name="name">
+    The name in context is: {{name}}
   </ng-template>
+  <ng-container [ngTemplateOutlet]="template2" [ngTemplateOutletContext]="{name: 'toto'}">
+    Container info...
+  </ng-container>
+
+  <h1>Renderer2</h1>
+  <button (click)="changeColor()">Change Color</button>
   `,
 })
 export class App {
   name = 'Angular';
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+
+  changeColor() {
+    const button = this.elementRef.nativeElement.querySelector('button');
+    this.renderer.setStyle(button, 'background-color', 'blue');
+    this.renderer.setStyle(button, 'color', 'white');
+  }
 }
 
 bootstrapApplication(App);
